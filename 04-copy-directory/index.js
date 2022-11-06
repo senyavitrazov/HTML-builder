1c9156f94649947e1d2d412c13a1e12960117aa4
vitrazov2 <arsenii2509@gmail.com>
2022-11-06 15:16:15 +0300

    feat: fin 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const copy = path.join(__dirname, 'files-copy');
+const folder = path.join(__dirname, 'files');
 
+async function copyFolder() {
+  await fs.promises.rm(copy, { recursive: true, force: true });
+
+  fs.mkdir(copy, { recursive: true }, (e) => {
+    if (e) throw e;
+  });
+
+  fs.readdir(folder, (e, data) => {
+    if (e) throw e;
+    data.forEach(file => { fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), (e) => { if (e) throw e; }); });
+  });
+  console.log('success!');
+}
+
+copyFolder();
